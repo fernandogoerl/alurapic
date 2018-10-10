95262a682b9b4a6a99784331c09958fd7166a17f
Fernando Goerl <fernandogoerl@gmail.com>
2018-10-10 20:03:46 -0300

    feat: consume photos from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { PhotosModule } from './photos/photos.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
