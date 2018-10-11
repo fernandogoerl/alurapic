e7a0718cca689a8d9ddc321e8bae43ea8986217d
Fernando Goerl <fernandogoerl@gmail.com>
2018-10-11 17:20:31 -0300

    feat: pagination on photo list

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { Photo } from './photo';
 
 const API = 'http://localhost:3000'
@@ -7,10 +8,19 @@ const API = 'http://localhost:3000'
 @Injectable({providedIn: 'root'})
 export class PhotoService {
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    listFromUser(username: string) {
-        return this.http
-            .get<Photo[]>(`${API}/${username}/photos`);
-    }
+  listFromUser(username: string) {
+    return this.http
+      .get<Photo[]>(API + '/' + username + '/photos');
+  }
+  
+  listFromUserPaginated(username: string, page: number) {
+    const params = new HttpParams().append('page', page.toString());
+    console.log(params.toString());
+    
+
+    return this.http
+      .get<Photo[]>(API + '/' + username + '/photos', { params });
+  }
 }
