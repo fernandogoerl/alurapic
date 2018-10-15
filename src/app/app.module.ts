b939bc3f7a91cb084bef81c8f685f006d09729a0
Fernando Goerl <fernandogoerl@gmail.com>
2018-10-15 15:46:07 -0300

    feat: new Home with SignInComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { ErrorsModule } from './errors/errors.module';
     PhotosModule,
     AppRoutingModule,
     ErrorsModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { SignInComponent } from './signin/signin.component';
+
+@NgModule({
+  declarations: [ SignInComponent ],
+  imports: [ 
+    CommonModule,
+    ReactiveFormsModule,
+ ],
+  exports: [],
+  providers: [],
+})
+export class HomeModule {}
