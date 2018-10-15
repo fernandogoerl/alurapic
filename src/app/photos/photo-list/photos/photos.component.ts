407d8df88842711e5993784e9b1be5351ba6d42c
Fernando Goerl <fernandogoerl@gmail.com>
2018-10-15 09:51:12 -0300

    fix: limited load of 12 photos per page

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -23,7 +23,7 @@ export class PhotosComponent implements OnChanges {
   groupColumns = (photos: Photo[]) => {
     const newRows = [];
 
-    for (let index = 0; index < photos.length; index++) {
+    for (let index = 0; index < photos.length; index+=3) {
       newRows.push(photos.slice(index, index+3));
     }
     return newRows
