85fb0768f169ce687f79983e4c432281aa9087c1
Fernando Goerl <fernandogoerl@gmail.com>
2018-10-17 13:07:53 -0300

    feat: new feature logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Observable } from 'rxjs';
 import { User } from '../user/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ap-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
   user$: Observable<User>;
-  user: User;
 
-  constructor(userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    ) {
     this.user$ = userService.getUser();
-    this.user$.subscribe(user => this.user = user)
   }
 
-  ngOnInit(): void { }
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['']);
+  }
 }
