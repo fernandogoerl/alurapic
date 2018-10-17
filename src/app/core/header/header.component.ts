e77750b3b1dfebbbfd17ed9cbb91f24824d98d3b
Fernando Goerl <fernandogoerl@gmail.com>
2018-10-17 12:53:02 -0300

    feat: header get username from token

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
 
 @Component({
   selector: 'ap-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
+
+  user$: Observable<User>;
+  user: User;
+
+  constructor(userService: UserService) {
+    this.user$ = userService.getUser();
+    this.user$.subscribe(user => this.user = user)
+  }
 
   ngOnInit(): void { }
 }
