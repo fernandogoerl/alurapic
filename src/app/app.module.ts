d5a3c0504e2ced7e1b716dcfc2e52044353756ec
Fernando Goerl <fernandogoerl@gmail.com>
2018-10-17 17:11:11 -0300

    feat: lazy loading HomeModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,10 +15,9 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     PhotosModule,
-    AppRoutingModule,
     ErrorsModule,
-    HomeModule,
     CoreModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
