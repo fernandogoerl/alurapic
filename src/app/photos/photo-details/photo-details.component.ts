da80f83a63efacbb1d75f2428b0d4356ee6eca97
Fernando Goerl <fernandogoerl@gmail.com>
2018-10-18 17:09:13 -0300

    feat: new Photo Details Component

---
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { PhotoService } from '../photo/photo.service';
+import { Photo } from '../photo/photo';
+
+@Component({
+  templateUrl: './photo-details.component.html',
+  styleUrls: ['photo-details.css']
+})
+export class PhotoDetailsComponent implements OnInit {
+
+  photo$: Observable<Photo>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private photoService: PhotoService,
+  ) { }
+
+  ngOnInit(): void {
+    this.photo$ = this.photoService.findById(
+      this.route.snapshot.params.photoId
+    );
+
+  }
+}
